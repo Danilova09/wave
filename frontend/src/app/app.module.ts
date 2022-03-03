9ec96bde732168d18986bfa1b8ae093585356d24
Danilova09 <93181066+Danilova09@users.noreply.github.com>
2022-03-03 17:43:50 +0600

    Generation of main components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,17 +6,39 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
+import { ToolbarComponent } from './pages/toolbar/toolbar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { ArtistsComponent } from './pages/artists/artists.component';
+import { ArtistItemComponent } from './pages/artists/artist-item/artist-item.component';
+import { AlbumsComponent } from './pages/albums/albums.component';
+import { AlbumItemComponent } from './pages/albums/album-item/album-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    ArtistsComponent,
+    ArtistItemComponent,
+    AlbumsComponent,
+    AlbumItemComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
     StoreModule.forRoot({}, {}),
-    EffectsModule.forRoot([])
+    EffectsModule.forRoot([]),
   ],
   providers: [],
   bootstrap: [AppComponent]
