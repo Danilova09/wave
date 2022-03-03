188c83bcdcce6ff325968f3bd81138cf8f3ae84b
Danilova09 <93181066+Danilova09@users.noreply.github.com>
2022-03-03 16:21:28 +0600

    Realization of getting and posting albums

--- api/index.js
+++ api/index.js
@@ -4,12 +4,14 @@ const mongoose = require('mongoose');
 const app = express();
 const config = require('./config');
 const artists = require('./app/artists');
+const albums = require('./app/albums');
 const port = 8000;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 app.use('/artists', (artists));
+app.use('/albums', (albums));
 
 const run = async () => {
     await mongoose.connect(config.mongo.db, config.mongo.options);
@@ -24,4 +26,8 @@ const run = async () => {
 }
 
 run().catch(e => console.log(e));
+
+
+
+
 
